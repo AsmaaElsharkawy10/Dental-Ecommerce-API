1dbbe39fe3af414f71b886b25261132b99aeb9a5
unknown <bishohany4@gmail.com>
2022-03-08 22:23:36 +0200

    update models

--- Models/ordersSchema.js
+++ Models/ordersSchema.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
+const autoIncrement=require('mongoose-sequence')(mongoose);
+
 
 const ordersSchema = new mongoose.Schema({
   _id: Number,
-  customerId: [{ type: Number, ref: 'customers' }],
+  customerId: [{ type: Number, ref: 'customers',required:true  }],
   status: {
     type: String,
     enum: ['inProgress', 'completed', 'underRevison'],
     required: true,
   },
-  addressId: { type: Number, ref: 'Addresses' },
-  storesId: { type: Number, ref: 'Stores' },
-  flyboyId: { type: Number, ref: 'Flyboy' },
+  addressId: { type: Number, ref: 'Addresses',required:true },
+  storesId: { type: Number, ref: 'Stores' ,required:true },
+  flyboyId: { type: Number, ref: 'Flyboy',required:true  },
   ordersDate: {
-    requestDate: { type: Date },
-    deliverDate: { type: Date },
+    requestDate: { type: Date ,required:true },
+    deliverDate: { type: Date ,required:true },
   },
 });
 
 const Orders = mongoose.model('Orders', ordersSchema);
 
+ordersSchema.plugin(autoIncrement, { inc_field: "_id" });
 module.exports = Orders;
