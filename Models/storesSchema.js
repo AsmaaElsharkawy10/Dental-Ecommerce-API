b2e72bb635cf46819627a3180c60007c8b3743eb
nada-medhat229 <nda96642@gmail.com>
2022-04-22 22:59:50 +0200

    edit store

--- Models/storesSchema.js
+++ Models/storesSchema.js
@@ -5,14 +5,12 @@ const storeSchema = new mongoose.Schema({
     _id: { type: Number, alias: "storeId" }, 
     storeName: {type:String , required:true},
     storePhone: String,
-    storeAddress: {
-    storeCity: { type: String },
-    storeStreet: { type: String },
-    },
+    storeAddress:{type:{ storeCity:String,storeStreet:String}},
+
     storeRent:String,
-    storeEmployeesId:[ { type: Number, ref: "employees", required:true}],
-    storeCategoriesId: [{ type: Number, ref: "categories", required:true}],
-    returnedProductsId:[{type:Number, ref:"returnsProudcts" }]
+    storeEmployeesId:[ { type: Number, ref: "Employees", required:true}],
+    storeCategoriesId: [{ type: Number, ref: "Category", required:true}],
+    // returnedProductsId:[{type:Number, ref:"returnsProudcts" }]
 })
 
 storeSchema.plugin(AutoIncrementId, { inc_field: "storeId" });
